61d1a60ef923e152e2717854a29099dcd6131f63
Daniel Matos <deme12.1@gmail.com>
2023-12-31 10:01:11 -0500

    Add docker configuration to preparing for deploy

--- inventario/src/main/java/com/odettelane/inventario/controller/SizeController.java
+++ inventario/src/main/java/com/odettelane/inventario/controller/SizeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sizes")
+@CrossOrigin
 public class SizeController {
     private final SizeService sizeService;
 
